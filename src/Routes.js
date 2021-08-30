d4a2513f3c18a79e4af841562483b528cf782daa
Casey <hjkdw9506@gmail.com>
2021-08-30 18:04:40 +0900

    🎉 Add : 초기 셋팅 설정 완료

---
+++ src/Routes.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Main from "./pages/Main/Main";
+import Product from "./pages/Product/Product";
+import Cart from "./pages/Cart/Cart";
+
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/product" component={Product} />
+          <Route exact path="/cart" component={Cart} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
