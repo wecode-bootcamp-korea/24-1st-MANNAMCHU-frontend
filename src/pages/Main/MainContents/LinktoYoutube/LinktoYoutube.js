56423b369f9214363591549ca2dba0cd2e1d896a
Casey <hjkdw9506@gmail.com>
2021-08-31 09:36:14 +0900

    ✨ Add : youtube연결 component 완성 - mp4파일 가져오는 부분은 draft로 추후 fetch로 받아오게끔 수정 예정

--- src/pages/Main/MainContents/LinktoYoutube/LinktoYoutube.js
+++ src/pages/Main/MainContents/LinktoYoutube/LinktoYoutube.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
+import "./LinktoYoutube.scss";
 
 export default class LinktoYoutube extends Component {
   render() {
     return (
-      <div className="linkToYoutube">
+      <div className="linktoYoutube letter">
+        <video muted autoPlay loop>
+          <source src="/sources/videos/foods.mp4" type="video/mp4" />
+        </video>
         <a href="https://www.youtube.com/watch?v=nLf6r5p8nqs">
-          <div>
+          <div className="content">
             마음을 담아 마음을 주는
             <br />
             프리미엄 과일(Fruits) 부티크
             <br />
             마담주 Madamjoo
           </div>
-          <div>
+          <div className="content">
             깐깐하게 고르고 품격 있게 담아
             <br />
             가장 신선한 행복을 전해드리는
