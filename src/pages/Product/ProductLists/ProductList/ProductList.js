e2694845507bf82d8ec0c555bfc63e1fd6217645
Casey <hjkdw9506@gmail.com>
2021-09-02 16:56:46 +0900

    ✨ Add: mockData 생성 및 component 연결 완료

--- src/pages/Product/ProductLists/ProductList/ProductList.js
+++ src/pages/Product/ProductLists/ProductList/ProductList.js
@@ -3,28 +3,31 @@ import "./ProductList.scss";
 
 export default class ProductList extends Component {
   render() {
+    const { item, discountRatio } = this.props;
     return (
       <li className="productList">
         <img
-          src="https://cdn.imweb.me/thumbnail/20210703/234bfb01c89a5.jpg"
+          src={item.thumbnails[0].first}
           alt="product_image"
           className="productImg"
         />
         <div className="letterWrapper">
-          <strong className="title letter">
-            [반짝할인] 제주 자연산 민어 사시미/지리/매운탕
-          </strong>
+          <strong className="title letter">{item.name}</strong>
           <div className="priceWrapper original">
-            <span className="amount">75,000</span>
+            <span className="amount">{item.price}</span>
             <span className="currency">원</span>
           </div>
           <div className="priceWrapper discount">
-            <span className="amount">67,500</span>
+            <span className="amount">
+              {item.discounted === true
+                ? (1 - discountRatio) * item.price
+                : item.price}
+            </span>
             <span className="currency">원</span>
           </div>
           <div className="likes">
             <i className="far fa-heart" />
-            <span>3</span>
+            <span>{item.likeCount}</span>
           </div>
         </div>
       </li>
--- src/pages/Product/ProductLists/ProductLists.js
+++ src/pages/Product/ProductLists/ProductLists.js
@@ -2,11 +2,30 @@ import React, { Component } from "react";
 import ProductList from "./ProductList/ProductList";
 
 export default class ProductLists extends Component {
+  state = {};
+
+  componentDidMount() {
+    fetch("/data/listData.json", {
+      method: "GET",
+    })
+      .then(res => res.json())
+      .then(data => this.setState(data));
+  }
+
   render() {
+    console.log(this.state);
+    const { discountRatio, listData } = this.state;
     return (
       <>
         <ul>
-          <ProductList />
+          {listData &&
+            listData.map(item => (
+              <ProductList
+                key={item.id}
+                item={item}
+                discountRatio={discountRatio}
+              />
+            ))}
         </ul>
       </>
     );
