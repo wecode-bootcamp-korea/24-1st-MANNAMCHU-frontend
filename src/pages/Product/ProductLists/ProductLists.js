3f86edeeb8ff3a5245cf50f8aa2ad637fcfac1c3
Casey <hjkdw9506@gmail.com>
2021-09-02 21:32:12 +0900

    🚧 Add : productlst filter 리모콘 ul 구현 완료

--- src/pages/Product/ProductLists/ProductLists.js
+++ src/pages/Product/ProductLists/ProductLists.js
@@ -1,22 +1,55 @@
 import React, { Component } from "react";
 import ProductList from "./ProductList/ProductList";
+import "./ProductLists.scss";
 
 export default class ProductLists extends Component {
-  state = {};
+  state = {
+    items: {},
+    fliter: "",
+  };
 
-  componentDidMount() {
+  handleFetch = () => {
     fetch("/data/listData.json", {
       method: "GET",
     })
       .then(res => res.json())
-      .then(data => this.setState(data));
+      .then(items => this.setState({ items }));
+  };
+
+  handleClick = e => {
+    const clickedName = e.target.className;
+    const filter = { filter: clickedName };
+    this.setState({ ...this.state, filter });
+  };
+
+  componentDidMount() {
+    this.handleFetch();
   }
 
   render() {
-    console.log(this.state);
-    const { discountRatio, listData } = this.state;
+    const { discountRatio, listData } = this.state.items;
+    const { filter } = this.state;
     return (
       <>
+        <div className="filterWrapper">
+          <ul className="filters">
+            <li className={`filter ${filter === "new" ? "active" : ""}`}>
+              <button className="new" onClick={this.handleClick}>
+                ▼ 신상품 순
+              </button>
+            </li>
+            <li className={`filter ${filter === "hot" ? "active" : ""}`}>
+              <button className="hot" onClick={this.handleClick}>
+                ▼ 인기 상품 순
+              </button>
+            </li>
+            <li className={`filter ${filter === "sale" ? "active" : ""}`}>
+              <button className="sale" onClick={this.handleClick}>
+                ▼ 할인 제품
+              </button>
+            </li>
+          </ul>
+        </div>
         <ul>
           {listData &&
             listData.map(item => (
@@ -24,6 +57,7 @@ export default class ProductLists extends Component {
                 key={item.id}
                 item={item}
                 discountRatio={discountRatio}
+                filter={filter}
               />
             ))}
         </ul>
