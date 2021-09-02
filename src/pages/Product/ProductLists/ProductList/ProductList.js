eae2c6c3e680fa0b73f333d2cbdca023b7c1195b
Casey <hjkdw9506@gmail.com>
2021-09-02 16:07:23 +0900

    💄 Add : list component UI 작업 완료

--- src/pages/Product/ProductLists/ProductList/ProductList.js
+++ src/pages/Product/ProductLists/ProductList/ProductList.js
@@ -8,19 +8,24 @@ export default class ProductList extends Component {
         <img
           src="https://cdn.imweb.me/thumbnail/20210703/234bfb01c89a5.jpg"
           alt="product_image"
+          className="productImg"
         />
         <div className="letterWrapper">
           <strong className="title letter">
             [반짝할인] 제주 자연산 민어 사시미/지리/매운탕
           </strong>
-          <span className="priceWrapper">
-            <span className="listPrice">75,000</span>
+          <div className="priceWrapper original">
+            <span className="amount">75,000</span>
             <span className="currency">원</span>
-          </span>
-          <span className="priceWrapper">
-            <span className="discounted">67,500</span>
+          </div>
+          <div className="priceWrapper discount">
+            <span className="amount">67,500</span>
             <span className="currency">원</span>
-          </span>
+          </div>
+          <div className="likes">
+            <i className="far fa-heart" />
+            <span>3</span>
+          </div>
         </div>
       </li>
     );
