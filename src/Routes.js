ec9ee9761e8a4d11b8954b4737b13283cc0febf3
mai <mai@maiui-MacBookAir.local>
2021-09-02 11:40:57 +0900

    Modify: 잘못된 branch에서 작업한 부분 원상복구했습니다.

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import Main from "./pages/Main/Main";
 import Product from "./pages/Product/Product";
 import Cart from "./pages/Cart/Cart";
 import Login from "./pages/Sign/Login/Login";
+import Signup from "./pages/Sign/Signup/Signup";
 import Footer from "./components/Footer/Footer";
 
 class Routes extends Component {
@@ -16,6 +17,7 @@ class Routes extends Component {
           <Route exact path="/product" component={Product} />
           <Route exact path="/cart" component={Cart} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
         </Switch>
         <Footer />
       </Router>
