cf7c0ee1434dda8afcea6655720129006a3524b5
choseonghwan91 <82944360+choseonghwan91@users.noreply.github.com>
2021-09-02 15:14:26 +0900

    Test commit

--- src/pages/Product/ProductDetail/ProductDetailComponent/InfoConfig/InfoConfigSelOpt/InfoConfigSelOpt.js
+++ src/pages/Product/ProductDetail/ProductDetailComponent/InfoConfig/InfoConfigSelOpt/InfoConfigSelOpt.js
@@ -2,9 +2,12 @@ import React, { Component } from "react";
 import "./InfoConfigSelOpt.scss";
 
 export default class InfoConfigSelOpt extends Component {
+  handleClick = e => {
+    this.props.addCartData(e);
+  };
   render() {
     return (
-      <button className="infoConfigSelOptList" onClick={this.props.addCartData}>
+      <button className="infoConfigSelOptList" onClick={this.handleClick}>
         {this.props.optList}
       </button>
     );
