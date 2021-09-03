b904a668db6ee7d5f0f7cf57ca92ba07e25b680b
Casey <hjkdw9506@gmail.com>
2021-09-03 15:52:52 +0900

    🚧 Add : header 작업 중

--- src/pages/Product/ListHeader/ListHeader.js
+++ src/pages/Product/ListHeader/ListHeader.js
@@ -6,15 +6,15 @@ export default class ListHeader extends Component {
     return (
       <header className="listHeader letter">
         <h1 className="logo title">Market</h1>
-        <h2>실패 없이 맛있는 과일 구매</h2>
+        <h2 className="titleDetail">실패 없이 맛있는 과일 구매</h2>
         <div className="explanation">
           <div className="explanationIntro">
-            <p>마담주가 직접 수매한 맛있는 과일을</p>
+            <span>마담주가 직접 수매한 맛있는 과일을 </span>
             <strong>새벽배송/전국택배</strong>
-            <p>로 만나보는</p>
+            <span>로 만나보는</span>
           </div>
           <div className="explanationCore">
-            <p>합리적 플랫폼,</p>
+            <span>합리적 플랫폼, </span>
             <strong>마담주 마켓</strong>
           </div>
         </div>
