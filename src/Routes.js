fd70747c2675fa6eb5a880770260b87bdfbaa85e
mai <mai@maiui-MacBookAir.local>
2021-09-03 16:16:31 +0900

    Modify: 화면 확인을 위한Signup  Routes 추가

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./pages/Main/Main";
 import Product from "./pages/Product/Product";
 import Cart from "./pages/Cart/Cart";
+import Signup from "./pages/Sign/Signup/Signup";
 import Footer from "./components/Footer/Footer";
 
 class Routes extends Component {
@@ -14,6 +15,7 @@ class Routes extends Component {
           <Route exact path="/" component={Main} />
           <Route exact path="/product" component={Product} />
           <Route exact path="/cart" component={Cart} />
+          <Route exact path="/signup" component={Signup} />
         </Switch>
         <Footer />
       </Router>
