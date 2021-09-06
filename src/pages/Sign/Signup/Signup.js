f6b500a199f7bac3d46967e0a3e101ed5b68aafd
Haeng-un Jeong <happycloudinmai@gmail.com>
2021-09-06 09:46:05 +0900

    Fix: '가입하기' 버튼 opacity 적용 안 되는 부분 개선

--- src/pages/Sign/Signup/Signup.js
+++ src/pages/Sign/Signup/Signup.js
@@ -62,9 +62,9 @@ class Signup extends Component {
     return (
       emailVal.includes("@") &&
       pwVal.length >= 8 &&
-      nameVal >= 2 &&
-      phoneVal >= 10 &&
-      addressVal >= 4
+      nameVal.length >= 2 &&
+      phoneVal.length >= 10 &&
+      addressVal.length >= 4
     );
   };
 
@@ -94,91 +94,95 @@ class Signup extends Component {
     const {
       emailVal,
       pwVal,
-      pwCheckVal,
+      // pwCheckVal,
       nameVal,
       phoneVal,
       addressVal,
-      addressCheckVal,
+      // addressCheckVal,
     } = this.state;
     return (
-      <div className="signupContainer">
-        <h1>회원 가입</h1>
-        <h3>계정 정보</h3>
-        <input
-          type="text"
-          placeholder="이메일"
-          className="inputEmail"
-          onChange={this.handleEmailInput}
-        />
-        {emailVal && !emailVal.includes("@") && (
-          <div className="warning">
-            이메일 형식에 맞게 입력해 주세요. ex '@' 포함
-          </div>
-        )}
-        <input
-          type="password"
-          placeholder="비밀번호"
-          className="inputPw"
-          onChange={this.handlePwInput}
-        />
-        {pwVal && pwVal.length < 8 && (
-          <div className="warning">8글자 이상 입력해 주세요.</div>
-        )}
-        <input
-          type="password"
-          placeholder="비밀번호 확인"
-          className="inputPwCheck"
-          onChange={this.handlePwCheckInput}
-        />
-        {/* {!pwCheckVal === pwVal && (
+      <div className="signup letter show-modal">
+        <div className="signupBackground"></div>
+        <div className="signupContainer">
+          <h1>회원 가입</h1>
+          <h3>계정 정보</h3>
+          <input
+            type="text"
+            placeholder="이메일"
+            className="inputEmail"
+            onChange={this.handleEmailInput}
+          />
+          {emailVal && !emailVal.includes("@") && (
+            <div className="warning">
+              이메일 형식에 맞게 입력해 주세요. ex '@' 포함
+            </div>
+          )}
+          <input
+            type="password"
+            placeholder="비밀번호"
+            className="inputPw"
+            onChange={this.handlePwInput}
+          />
+          {pwVal && pwVal.length < 8 && (
+            <div className="warning">8글자 이상 입력해 주세요.</div>
+          )}
+          <input
+            type="password"
+            placeholder="비밀번호 확인"
+            className="inputPwCheck"
+            onChange={this.handlePwCheckInput}
+          />
+          {/* {!pwCheckVal === pwVal && (
           <div className="warning">(비밀번호 확인과 관련한 문구 필요)</div>
         )} */}
-        <h3>가입 정보</h3>
-        <input
-          type="text"
-          placeholder="이름"
-          className="inputName"
-          onChange={this.handleNameInput}
-        />
-        {nameVal && nameVal.length < 2 && (
-          <div className="warning">이름을 입력해 주세요.</div>
-        )}
-        <input
-          type="number"
-          placeholder="휴대전화번호"
-          className="inputPhone"
-          onChange={this.handlePhoneInput}
-        />
-        {phoneVal && phoneVal.length < 10 && (
-          <div className="warning">
-            휴대전화 번호를 입력해 주세요. ex 01033334444
+          <h3>가입 정보</h3>
+          <input
+            type="text"
+            placeholder="이름"
+            className="inputName"
+            onChange={this.handleNameInput}
+          />
+          {nameVal && nameVal.length < 2 && (
+            <div className="warning">이름을 입력해 주세요.</div>
+          )}
+          <input
+            type="number"
+            placeholder="휴대전화번호"
+            className="inputPhone"
+            onChange={this.handlePhoneInput}
+          />
+          {phoneVal && phoneVal.length < 10 && (
+            <div className="warning">
+              휴대전화 번호를 입력해 주세요. ex 01033334444
+            </div>
+          )}
+          <input
+            type="text"
+            placeholder="주소"
+            className="inputAddress"
+            onChange={this.handleAddressInput}
+          />
+          <input
+            type="text"
+            placeholder="상세 주소"
+            className="inputAddressDetail"
+            onChange={this.handleAddressDetailInput}
+          />
+          {addressVal && addressVal.length < 4 && (
+            <div className="warning">주소를 입력해 주세요.</div>
+          )}{" "}
+          <button
+            className={this.checkValid() ? "signupBtn" : "signupBtn disabled"}
+            type="button"
+            onClick={this.handleSignup}
+          >
+            가입하기
+          </button>
+          <div className="copyRight">
+            © mannamchu, Co., Ltd.. All Rights Free
           </div>
-        )}
-        <input
-          type="text"
-          placeholder="주소"
-          className="inputAddress"
-          onChange={this.handleAddressInput}
-        />
-        <input
-          type="text"
-          placeholder="상세 주소"
-          className="inputAddressDetail"
-          onChange={this.handleAddressDetailInput}
-        />
-        {addressVal && addressVal.length < 4 && (
-          <div className="warning">주소를 입력해 주세요.</div>
-        )}{" "}
-        <button
-          className={this.checkValid() ? "signupBtn" : "signupBtn disabled"}
-          type="button"
-          onClick={this.handleSignup}
-        >
-          가입하기
-        </button>
-        <div className="copyRight">© mannamchu, Co., Ltd.. All Rights Free</div>
+        </div>
       </div>
-      // 오른쪽 상단에 x 버튼 추가해야 함
     );
   }
 }
