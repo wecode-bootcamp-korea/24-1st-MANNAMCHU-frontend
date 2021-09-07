7ba739be33f2d0706545c1cd2e00086597b1df6a
choseonghwan91 <82944360+choseonghwan91@users.noreply.github.com>
2021-09-08 01:31:59 +0900

    Fix : 장바구니 페이지 서버 연결에 필요한 키값 수정

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Product from "./CartComponent/Product/Product";
+import WishList from "./CartComponent/WishList/WishList";
 import "./Cart.scss";
 
 export default class Cart extends Component {
@@ -10,8 +12,25 @@ export default class Cart extends Component {
     };
   }
 
+  componentDidMount = () => {
+    fetch("/data/listData.json")
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          cartData: response,
+        });
+      });
+  };
+
+  //서버와 통신
   // componentDidMount = () => {
-  //   fetch("/data/listData.json")
+  //   fetch("http://10.58.4.175:8000/products/cart", {
+  //     method: "GET",
+  //     headers: {
+  //       Authorization:
+  //         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.gkCFFEj525ntZJdHEqKnUD8X-v9oy75acVxJrqqtpZE",
+  //     },
+  //   })
   //     .then(response => response.json())
   //     .then(response => {
   //       this.setState({
@@ -20,28 +39,14 @@ export default class Cart extends Component {
   //     });
   // };
 
-  componentDidMount = () => {
-    fetch("http://10.58.4.175:8000/products/cart", {
-      method: "GET",
-      headers: {
-        Authorization:
-          "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.gkCFFEj525ntZJdHEqKnUD8X-v9oy75acVxJrqqtpZE",
-      },
-    })
-      .then(response => response.json())
-      .then(response => {
-        this.setState({
-          cartData: response,
-        });
-
-        console.log(this.state.cartData);
-      });
-  };
-
   addTotalPrice = () => {
     let addTotalPrice = 0;
-    for (let i = 0; i < this.state.cartData.listData.length; i++) {
-      addTotalPrice = addTotalPrice + this.state.cartData.listData[i].price;
+    for (let i = 0; i < this.state.cartData.message.length; i++) {
+      addTotalPrice =
+        addTotalPrice +
+        (parseInt(this.state.cartData.message[i].price) +
+          parseInt(this.state.cartData.message[i].addtional_price)) *
+          this.state.cartData.message[i].quantity;
     }
     return addTotalPrice;
   };
@@ -52,7 +57,7 @@ export default class Cart extends Component {
         <div className="cartTop">
           <div className="cartHead letter">장바구니</div>
           <div className="cartCount purple">
-            {this.state.cartData.listData?.length}
+            {this.state.cartData.message?.length}
           </div>
         </div>
         <div className="product">
@@ -69,9 +74,15 @@ export default class Cart extends Component {
             <li className="productBtn letter"></li>
           </ul>
         </div>
-        {this.state.cartData.listData &&
-          this.state.cartData.listData.map((listData, idx) => {
-            return <Product key={idx} listData={listData} />;
+        {this.state.cartData.message &&
+          this.state.cartData.message.map((listData, idx) => {
+            return (
+              <Product
+                key={idx}
+                listData={listData}
+                postPrice={this.state.postPrice}
+              />
+            );
           })}
         <div className="itemTotal">
           <div className="itemTotalTitle">
@@ -81,18 +92,18 @@ export default class Cart extends Component {
           </div>
           <div className="itemTotalContent">
             <span className="letter">
-              {this.state.cartData.listData &&
+              {this.state.cartData.message &&
                 this.addTotalPrice().toLocaleString()}
               원
             </span>
             <span className="letter">
-              {this.state.cartData.listData &&
+              {this.state.cartData.message &&
                 (this.addTotalPrice() > this.state.postPrice
                   ? "무료"
                   : "3,000원")}
             </span>
             <span className="letter">
-              {this.state.cartData.listData && this.addTotalPrice() / 100}
+              {this.state.cartData.message && this.addTotalPrice() / 100}
               포인트
             </span>
           </div>
@@ -105,7 +116,7 @@ export default class Cart extends Component {
           <div className="cartSelectTotal">
             <span className="cartSelectTotalText letter">결제금액</span>
             <span className="cartSelectTotalPrice logo">
-              {this.state.cartData.listData &&
+              {this.state.cartData.message &&
                 (
                   (this.addTotalPrice() > this.state.postPrice ? 0 : 3000) +
                   this.addTotalPrice()
@@ -132,74 +143,92 @@ export default class Cart extends Component {
   }
 }
 
-class Product extends Component {
-  render() {
-    return (
-      <div className="product">
-        <ul className="productList">
-          <li className="productCheck">
-            <input type="checkbox" className="productCheckBtn" />
-          </li>
-          <li className="productItem">
-            <div>
-              <img
-                className="productItemImg"
-                alt="상품이미지"
-                src={this.props.listData.thumbnails[0].item}
-              />
-            </div>
-            <div>
-              <div className="productItemName letter">
-                {this.props.listData.name}
-              </div>
-              <div className="productItemOpt letter">상품옵션</div>
-            </div>
-          </li>
-          <li className="productWish">
-            <i className="far fa-heart" />
-          </li>
-          <li className="productCount">
-            <div className="productCountNum">1개</div>
-            <button className="productCountChange">변경</button>
-          </li>
-          <li className="productShip letter">택배</li>
-          <li className="productShipPrice letter">3000원</li>
-          <li className="productPrice letter">
-            {parseInt(this.props.listData.price).toLocaleString()}원
-          </li>
-          <li className="productBtn">
-            <button className="productOrder">주문</button>
-            <button className="productDelete">삭제</button>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+// class Product extends Component {
+//   render() {
+//     return (
+//       <div className="product">
+//         <ul className="productList">
+//           <li className="productCheck">
+//             <input type="checkbox" className="productCheckBtn" />
+//           </li>
+//           <li className="productItem">
+//             <div>
+//               <img
+//                 className="productItemImg"
+//                 alt="상품이미지"
+//                 src={this.props.listData.image}
+//               />
+//             </div>
+//             <div>
+//               <div className="productItemName letter">
+//                 {this.props.listData.product}
+//               </div>
+//               <div className="productItemOpt letter">
+//                 {this.props.listData.option}
+//               </div>
+//             </div>
+//           </li>
+//           <li className="productWish">
+//             <i className="far fa-heart" />
+//           </li>
+//           <li className="productCount">
+//             <div className="productCountNum">
+//               {this.props.listData.quantity}
+//             </div>
+//             <button className="productCountChange">변경</button>
+//           </li>
+//           <li className="productShip letter">택배</li>
+//           <li className="productShipPrice letter">
+//             {(parseInt(this.props.listData.price) +
+//               parseInt(this.props.listData.addtional_price)) *
+//               this.props.listData.quantity >
+//             this.props.postPrice
+//               ? "무료"
+//               : "3,000원"}
+//           </li>
+//           <li className="productPrice letter">
+//             {(
+//               (parseInt(this.props.listData.price) +
+//                 parseInt(this.props.listData.addtional_price)) *
+//               this.props.listData.quantity
+//             ).toLocaleString()}
+//             원
+//           </li>
+//           <li className="productBtn">
+//             <button className="productOrder">주문</button>
+//             <button className="productDelete">삭제</button>
+//           </li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
 
-class WishList extends Component {
-  render() {
-    return (
-      <div className="wishList">
-        <div className="wishListImg">
-          <img
-            alt="과일"
-            src="	https://cdn.imweb.me/thumbnail/20210328/a279ae183ff9e.jpg"
-          />
-        </div>
-        <div className="wishListName">[새벽배송] 메론스럽게,메론</div>
-        <div className="wishListPrice">
-          <span className="wishListPriceReal letter">64000원</span>
-          <span className="wishListPriceSale letter">31500원</span>
-        </div>
-        <div className="wishListTags">{/* <Tags /> */}</div>
-        <div className="wishListWish">
-          <span>
-            <i className="far fa-heart" />
-          </span>
-          <span className="wishListWishCount">2</span>
-        </div>
-      </div>
-    );
-  }
-}
+// class WishList extends Component {
+//   render() {
+//     return (
+//       <div className="wishList">
+//         <div className="wishListImg">
+//           <img
+//             alt="과일"
+//             src="	https://cdn.imweb.me/thumbnail/20210328/a279ae183ff9e.jpg"
+//           />
+//         </div>
+//         <div className="wishListName">[새벽배송] 메론스럽게,메론</div>
+//         <div className="wishListPrice">
+//           <span className="wishListPriceReal letter">64000원</span>
+//           <span className="wishListPriceSale letter">31500원</span>
+//         </div>
+//         <div className="wishListTags">
+//           <Tags isNew={true} isBest={true} isSale={true} />
+//         </div>
+//         <div className="wishListWish">
+//           <span>
+//             <i className="far fa-heart" />
+//           </span>
+//           <span className="wishListWishCount">2</span>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
---
+++ src/pages/Cart/CartComponent/Product/Product.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+
+export default class Product extends Component {
+  render() {
+    return (
+      <div className="product">
+        <ul className="productList">
+          <li className="productCheck">
+            <input type="checkbox" className="productCheckBtn" />
+          </li>
+          <li className="productItem">
+            <div>
+              <img
+                className="productItemImg"
+                alt="상품이미지"
+                src={this.props.listData.image}
+              />
+            </div>
+            <div>
+              <div className="productItemName letter">
+                {this.props.listData.product}
+              </div>
+              <div className="productItemOpt letter">
+                {this.props.listData.option}
+              </div>
+            </div>
+          </li>
+          <li className="productWish">
+            <i className="far fa-heart" />
+          </li>
+          <li className="productCount">
+            <div className="productCountNum">
+              {this.props.listData.quantity}
+            </div>
+            <button className="productCountChange">변경</button>
+          </li>
+          <li className="productShip letter">택배</li>
+          <li className="productShipPrice letter">
+            {(parseInt(this.props.listData.price) +
+              parseInt(this.props.listData.addtional_price)) *
+              this.props.listData.quantity >
+            this.props.postPrice
+              ? "무료"
+              : "3,000원"}
+          </li>
+          <li className="productPrice letter">
+            {(
+              (parseInt(this.props.listData.price) +
+                parseInt(this.props.listData.addtional_price)) *
+              this.props.listData.quantity
+            ).toLocaleString()}
+            원
+          </li>
+          <li className="productBtn">
+            <button className="productOrder">주문</button>
+            <button className="productDelete">삭제</button>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
